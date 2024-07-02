ee13981729ab82dcf4fee02b00ec8c836586767f
Oleg <sagdeev.ol@yandex.ru>
2024-07-02 17:21:26 +0300

    Update 6.2ZADACHA.py

--- 6.2ZADACHA.py
+++ 6.2ZADACHA.py
@@ -1,23 +1,24 @@
 class Vehicle:
     __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
-    __model = 'Toyota Mark II'
-    __engine_power = 500
-    __color = 'blue'
-    def __init__(self, owner):
+    def __init__(self, owner, __model, __engine_power, __color):
         self.owner = owner
+        self.__model = __model
+        self.__engine_power = __engine_power
+        self.__color = __color
+
     def get_model(self):
-        return f'Модель: {self.__model}; '
+        return f'Модель: {self.__model};'
 
     def get_horsepower(self):
-        return f'Лошадиные силы: {self.__engine_power}; '
+        return f'\nЛошадиные силы: {self.__engine_power}; '
 
     def get_color(self):
-        return f'Цвет: {self.__color}; '
+        return f'\nЦвет: {self.__color}; '
 
 
     def print_info(self):
         print(self.get_model(), self.get_horsepower(),
-              self.get_color(), f'Владелец: {self.owner}.')
+              self.get_color(), f'\nВладелец: {self.owner}.')
 
     def set_color(self, new_color):
         if new_color.lower() in self.__COLOR_VARIANTS:
@@ -31,7 +32,7 @@ class Sedan(Vehicle):
 
 
 # Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
-vehicle1 = Sedan('Fedos')
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, 'blue')
 #
 # Изначальные свойства
 vehicle1.print_info()
